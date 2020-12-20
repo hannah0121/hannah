a0a68299146be1ce80ba140b6286880bfdd426ba
hannah0121 <73317524+hannah0121@users.noreply.github.com>
2020-12-19 22:54:01 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,33 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+class bob();
+bobObject1
+bobObject2
+bobObject3
+bobObject4
+bobObject5
+
+class roof;
+
+class rope;
+
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
-	World.add(world, packageBody);
-	
-	
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	//Create the Bodies Here.
 
 
 	Engine.run(engine);
@@ -48,17 +38,22 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  
   drawSprites();
  
 }
 
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	Matter.Body.setStatic(packageBody, false);
+constructor(body1, body2, offsetX, offsetY)
+{
+	this.offsetX = offsetX
+	this.offsetY = offsetY
 
-    
-  }
+	var options={
+		bodyA: body1
+		bodyB: body2
+		pointB: {x: this.offsetX, y: this.offsetY}
+	}
+	//console.log(options);
+	this.rope = Constraint.create(options)
+	World.add(world, this.rope)
 }
